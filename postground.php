29a393e0bb1ae872ed9a92247d29a82460753d7b
Sadiq Abdulazeez <sadibest8@gmail.com>
2025-09-12 23:33:55 +0100

    updated added some new file and code

--- postground.php
+++ postground.php
@@ -5,57 +5,82 @@
    <meta charset="utf-8"/>
    <!-- <link rel="stylesheet" href="css/gen.css"/>
    <link rel="stylesheet" href="css/gen2.css"/> -->
-   <link rel="stylesheet" href="css/style.css"/>
    <link rel="stylesheet" href="bootstrap-5.3.8-dist/css/bootstrap.css">
+   <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
-<div id="" class="container-fluid bg-dark-subtle p-5">
+<div id="" class="container-fluid bg-dark-subtle p-5 ftpFormRoot">
+   <div class="contaier row p-5 card ftpFormContainer">
+        <div class="uploadFormContainer card-body">
+            <div class="closeBtnContainer"><button type="button" class="btn-close" disabled aria-label="Close"></button></div>
+            
+            <div class="uploadIntro container row p-0 m-0">
+                <div class="fileIcon">
+                    <i class="fa-regular fa-file fs-5"></i>
+                </div>
+                <div class="fileText row">
+                    <p class="i">Upload Files</p>
+                    <p class="i">Select and upload the files of your choice</p>
+                </div>
+            </div>
+            
+            <div class="uploadForm container p-5 m-0">
+                <div class="container row">
+                    <label class="smallFIileIcon" for="file_upload">
+                        <i class="fa-regular fa-cloud fs-5"></i>
+                    </label>
+                    <p class="mainFileUpload">
+                        <label class="lb lbSmall">Choose a file or drag & drop it here</label>
+                        <label class="lb">JPEG, PNG, PDG, and MP4 formats up to 50MB</label>
+                        <input type="file" class="btn files" id="file_upload" name="file_upload" width="100%" multiple />
+                    </p>
+                </div>
+            </div>
+        
+            <div class="progress_container container p-4 mt-3 bg-opacity-25 bg-dark border border-3 border-dark-subtl rounded-3">
+                <div class="pb-2 text-gray fs-5 fw-3">
+                    <div class="spinner-border spinner-border-sm"><span class="visually-hidden">Loading...</span></div>
+                    <span class="">Uploading...</span>
+                </div>
 
-   <div class="contaier bg-danger ">
-      <div id="logs_container" class="row p-5 card">
-            <div class="upload_form_container">
-            <div class="upload_form">
-                    <div class="min_divider">
-                        <label class="lb" for="file_upload">SELECT FILES TO UPLOAD:</label>
-                        <input type="file" class="files" id="file_upload" name="file_upload" width="100%" multiple />
-                        <input type="submit" value="POST" name="POST" class="post_files" id="Post_files">
+                <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: 90%">25%</div>
+                </div>
+            </div>
+
+            <div class="progress_status container mt-3 px-0" id="progress_status">
+                <div class="container-fluid fs-6 p-2 alert alert-danger alert-dismissible fade show d-flex align-items-center" role="alert">
+                    <div>
+                        <i class="fa-solid fa-triangle-exclamation"></i>
+                        An example success alert with an icon
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
-            </div>
-            </div>
-
-            <div class="progress_container">
-                <div class="progress_bar_container" id="progress_bar_container">
-                    <div class="progress_bar" id="progress_bar">0%</div>
-                </div>
-            </div>
-
-            <div class="progress_status" id="progress_status" width="100%">
-                <!-- <div class="container progress_status_container file_status_success" id= "progress_status_container" >
-                    <div class="progress_status file_status_success"> File Uploaded Successfuly </div>
                 </div>
 
-                <div class="container progress_status_container file_status_fail" id= "progress_status_container" >
-                    <div class="progress_status file_status_fail"> File Uploaded Fail </div>
-                </div> -->
+                <div class="container-fluid fs-6 p-2 alert alert-success alert-dismissible fade show d-flex align-items-center" role="alert">
+                    <div>
+                        <i class="fa-solid fa-check"></i>
+                        An example success alert with an icon
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
             </div>
-      </div>
-   </div>
-
-
-    <!-- FOOTER START -->
-        <footer id="contact" class="container-fluid contactsContainer py-5">
-            <div class="bag footer-content container p-5">
-            <p class="contactsIcon p-3 text-center d-block">
-                <a href="https://www.linkedin.com/in/sadiq-abdulazeez-687a1b350?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" class="smallIcons"><i class="fa-brands fa-linkedin smallIconLinkedin fs-2"></i></a>
-                <a href="https://wa.me/08104086611" class="smallIcons"><i class="fa-brands fa-whatsapp smallIconWhatsapp fs-2"></i></a>
-                <a href="https://www.pinterest.com/akonfex/" class="smallIcons"><i class="fa-brands fa-pinterest smallIconPinterest fs-2"></i></a>
-            </p>
-            <a href="#" class="copywrite d-block text-center text-light text-decoration-none">Develop with ❤ By Sadiq</a>
-            <p class="site_date text-center d-block">2021 - <?php echo date("Y", time())?></p>
-        </div>
-        </footer>
-    <!-- FOOTER END -->
+   </div>  
 </div>
+
+<!-- FOOTER START -->
+    <footer id="contact" class="container-fluid contactsContainer py-5">
+        <div class="bag footer-content container p-5">
+        <p class="contactsIcon p-3 text-center d-block">
+            <a href="https://www.linkedin.com/in/sadiq-abdulazeez-687a1b350?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" class="smallIcons"><i class="fa-brands fa-linkedin smallIconLinkedin fs-2"></i></a>
+            <a href="https://wa.me/08104086611" class="smallIcons"><i class="fa-brands fa-whatsapp smallIconWhatsapp fs-2"></i></a>
+            <a href="https://www.pinterest.com/akonfex/" class="smallIcons"><i class="fa-brands fa-pinterest smallIconPinterest fs-2"></i></a>
+        </p>
+        <a href="#" class="copywrite d-block text-center text-light text-decoration-none">Develop with ❤ By Sadiq</a>
+        <p class="site_date text-center d-block">2021 - <?php echo date("Y", time())?></p>
+    </div>
+    </footer>
+<!-- FOOTER END -->
 
 <script src="bootstrap-5.3.8-dist/js/bootstrap.js"></script>
 <script src="https://kit.fontawesome.com/1f1ec3e39a.js" crossorigin="anonymous"></script>
---
+++ script/app.js
@@ -0,0 +1,207 @@
+
+function byId(elemnt){
+    return document.getElementById(elemnt);
+}
+
+function byTag(elemnt){
+    return document.getElementsByTagName(elemnt);
+}
+
+function byClass(elemnt){
+    return document.getElementsByClassName(elemnt);
+}
+
+function byQuery(elemnt){
+    return document.querySelector(elemnt);
+}
+
+function byQueryAll(elemnt){
+    return document.querySelectorAll(elemnt);
+}
+
+function toConsole (elemnt){
+    console.log (elemnt);
+}
+
+
+function showMsg(parent, status, msg){
+    errorMsg = `
+        <div class="container progress_status_container ${status}" id="progress_status_container" >
+        <div class="progress_status file_status_fail"> ${msg} </div>
+        </div>
+    `;
+
+    byId(parent).innerHTML = errorMsg;
+
+    setTimeout(function(){
+        byId(parent).innerHTML = "";
+    }, 3000);
+}
+
+// Getting and checking if the Post Btn is click 
+byId('Post_files').onclick = function (evnt){
+
+    // creating and object of formData to hold file and details submitted
+    let form_data = new FormData();
+    
+    // initializing some variable 
+    let image_number = 1;
+    let errorMsg = '';
+
+    // Getting some UI Element 
+    const progressStatus = byId("progress_status");
+    const progressBarCon = byQuery(".progress_status_container");
+
+    // Getting and checking if the Post Btn is click and making sure file is not empty
+    if (byId('file_upload').files.length > 0){
+
+        for (let count = 0; count < byId('file_upload').files.length; count++){
+
+            if (!['image/jpeg', 'image/png', "audio/mpeg","video/mp4", "video/x-matroska", "video/quicktime", "video/avi" ].includes(
+                byId('file_upload').files[count].type)
+            ){    
+                showMsg('progress_status', 'file_status_fail', 'Invalid Image, video OR audio format. Uploading Fail');
+                byId('file_upload').value = '';
+                errorMsg = true;
+                //return false;
+            }else{
+                form_data.append("images[]", byId('file_upload').files[count]);
+                // showMsg('progress_status', 'file_status_success', 'File Uploaded Successfuly');
+                // form_data.forEach(function(elemnt){
+                //     toConsole(elemnt);
+                // });
+
+                toConsole(byId('file_upload').files[count]);
+            }
+
+            image_number++;
+        }
+
+        if (errorMsg !== ''){
+            showMsg('progress_status', 'file_status_fail', 'Oops..Inappropraite File Format');
+            byId('file_upload').value = '';
+        }else{
+            
+            byId('progress_bar_container').style.display = "block";
+            const xmlrequest = new XMLHttpRequest();
+            xmlrequest.open('POST', 'post.php', true);
+            xmlrequest.upload.addEventListener('progress', function (evnt){
+                // console.log(JSON.parse(this.responseText));
+                let percent_complete = Math.round((evnt.loaded / evnt.total) * 100);
+                byId('progress_bar').style.width = `${percent_complete}%`;
+                byId('progress_bar').style.backgroundColor =  `${percent_complete}%` == '100%' ? 'green' : 'red'; 
+                byId('progress_bar').innerHTML = percent_complete + " % completed";
+
+            });
+
+            showMsg('progress_status', 'file_status_success', 'File Uploaded Successfuly');
+            byId('file_upload').value = '';
+
+            xmlrequest.addEventListener('load', function(evnt){
+                // errorMsg = `
+                // <div class="container progress_status_container file_status_success" id= "progress_status_container" >
+                //     <div class="progress_status file_status_success"> File Uploaded Successfuly </div>
+                // </div>
+                // `;
+                // progressStatus.innerHTML = errorMsg;
+                // byId('file_upload').files.value = "";
+                // console.log(JSON.parse(this.responseText));
+                // xmlrequest.onreadystatechange = function(e){
+                //     if(this.readyState == 4 && this.status == 200){
+                //         
+                //     }
+                // }
+
+            });
+
+            xmlrequest.send(form_data);
+        }
+
+    }else {
+        showMsg('progress_status', 'file_status_fail', 'Oops...Please select file to upload');
+        byId('file_upload').value = '';
+        return false;
+    }
+
+}
+
+
+document.querySelector(".min_divider input[type='submit']").addEventListener("click", getForm);
+
+
+function getForm(e){
+    const filesObject = e.target.previousElementSibling.files;
+    let fileCounter = 0;
+    let number_of_file = 0;
+    let errorMsg = '';
+    let form_data = new FormData();
+    const progressStatus = document.getElementById("progress_status");
+    const progressBarCon = document.querySelector(".progress_status_container");
+    const progressBar = document.querySelector(".progress_bar");
+
+    for(fileCounter; fileCounter < filesObject.length; fileCounter++){
+        if (!['image/jpeg', 'image/png', 'audio/mpeg', 'video/mp4', 'video/x-matroska', 'video/quicktime', 'video/avi'].includes(
+            filesObject[fileCounter].type
+        )){    
+            errorMsg = `
+            <div class="container progress_status_container file_status_fail" id= "progress_status_container" >
+             <div class="progress_status file_status_fail"> Invalid Image, video OR audio format. Uploading Fail </div>
+            </div>
+             `;
+             progressStatus.innerHTML = errorMsg;
+            // console.log(msgBoxContainer);
+            // alert("Error");
+            console.log("Error");
+        }else{
+            form_data.append("files_upload[]", filesObject[fileCounter]);
+            number_of_file++;
+            // console.log(filesObject[fileCounter]);
+            console.log(form_data.getAll("files_upload"));
+
+            errorMsg = `
+            <div class="container progress_status_container file_status_success" id= "progress_status_container" >
+                <div class="progress_status file_status_success"> File Uploaded Successfuly </div>
+            </div>
+             `;
+             progressStatus.innerHTML = errorMsg;
+        }
+    }
+
+    // if (errorMsg == '' && number_of_file <= 0){
+    //     msgBoxContainer.style.display = "block";
+    //     e.target.previousElementSibling.value = "";
+    //     console.log(fileList);
+    // }else{
+        
+    //     // e.target.previousElementSibling.value = "";
+
+    //     const xml = new XMLHttpRequest();
+    //     xml.open("POST", "post.php");
+    //     xml.upload.addEventListener("progress", function(e){
+    //         progressBarCon.style.display = "block";
+    //         const thiss = e;
+    //         let upload_status = Math.round((thiss.loaded / thiss.total) * 100);
+    //         progressBar.style.width = upload_status +  " %";
+    //         console.log(upload_status);
+    //         // xml.onreadystatechange = function(e){
+    //         //     if(this.readyState == 4 && this.status == 200){
+    //         //             // if (){
+    //         //             let upload_status = Math.round(thiss.loaded / thiss.total * 100); 
+    //         //             progressBar.style.width = upload_status + " %";
+    //         //             progressBar.style.innerHTML = upload_status + " % Completed";
+    //         //             // }else{
+
+    //         //             // }
+
+    //         //             // console.log(JSON.parse(this.responseText));
+
+    //         //     }
+    //         // }
+    //     });
+
+    //     xml.send(form_data, true);
+    // }
+
+
+    e.preventDefault();
+}
