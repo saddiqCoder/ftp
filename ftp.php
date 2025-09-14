245862d3e90828f1c0b084bcae96672184e9e520
Sadiq Abdulazeez <sadibest8@gmail.com>
2025-09-14 23:25:20 +0100

    update of HEAD:

--- ftp.php
+++ ftp.php
@@ -3,16 +3,16 @@
 $msg = [];
 $msg['error'] = "true";
 
-if(isset($_FILES['images'])){
+if(isset($_FILES['files'])){
 
-    for($count = 0; $count < count($_FILES['images']['name']); $count++){
-        $ext = pathinfo($_FILES['images']['name'][$count], PATHINFO_EXTENSION);
+    for($count = 0; $count < count($_FILES['files']['name']); $count++){
+        $ext = pathinfo($_FILES['files']['name'][$count], PATHINFO_EXTENSION);
         $newfilename = uniqid() . '.' . $ext;
 
-        if (move_uploaded_file($_FILES['images']['tmp_name'][$count], 'img/'.$newfilename)){
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$count], 'img/'.$newfilename)){
             $msg['error'] = "false";
         }else{
-            $msg['error'] = $_FILES['images']['error'][$count];
+            $msg['error'] = $_FILES['files']['error'][$count];
         }
     }
 
